99f08aa2ea151f05b15f7851806eec5be1780dee
muralineerukonda <83264943+muralineerukonda@users.noreply.github.com>
2021-07-05 20:49:16 +0530

    Update hello_world.java

--- hello_world.java
+++ hello_world.java
@@ -1,5 +1,5 @@
 class hello_world {
 	public static void main (String args[]) {
-		System.out.println("Hello World!!! This is Murali. I am  the legend");
+		System.out.println("Hello World!!! This is Murali. I am  the legend. But failed to achieve legacy");
 	}
 }
